145ecd79dc1e20eed3bfc6981103dde91359205c
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2023-12-01 17:09:45 -0300

    Initial LoginScreen layout

--- app/src/main/java/com/ufape/shaypado/ui/routes/AuthNavigation.kt
+++ app/src/main/java/com/ufape/shaypado/ui/routes/AuthNavigation.kt
@@ -4,16 +4,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -23,13 +29,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -62,46 +71,94 @@ fun NavGraphBuilder.authNavGraph(navController: NavController) {
     }
 
 }
+
 @Preview
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RoundedCornerTextField() {
+fun CustomTextField(
+    keyboardType: KeyboardType = KeyboardType.Text,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    placeholder: String = ""
+) {
     var textState by remember { mutableStateOf("") }
-    val maxLength = 110
-    val blue = Color(0xff76a9ff)
+    val containerColor = colorResource(id = R.color.box_light)
     TextField(
         modifier = Modifier.fillMaxWidth(),
         value = textState,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
         onValueChange = { textState = it },
-        shape = RoundedCornerShape(8.dp),
+        shape = RoundedCornerShape(15.dp),
         singleLine = true,
-        colors = TextFieldDefaults.textFieldColors(
+        placeholder = { Text(placeholder) },
+        colors = TextFieldDefaults.colors(
             disabledTextColor = Color.Transparent,
+            focusedContainerColor = containerColor,
+            unfocusedContainerColor = containerColor,
+            disabledContainerColor = containerColor,
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent
+            disabledIndicatorColor = Color.Transparent,
         ),
-        leadingIcon = {
-            if (textState.isNotEmpty()) {
-                IconButton(onClick = { textState = "" }) {
-                    Icon(
-                        imageVector = Icons.Outlined.Close,
-                        contentDescription = null
-                    )
-                }
-            }
-        }
-    )
-    Text(
-        text = "${textState.length} / $maxLength",
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 4.dp),
-        textAlign = TextAlign.End,
-        color = blue
+        leadingIcon = leadingIcon,
     )
 }
 
+@Composable
+@Preview
+fun CustomPasswordTextField(
+    leadingIcon: @Composable (() -> Unit)? = null
+) {
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+
+    val containerColor = colorResource(id = R.color.box_light)
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        colors = TextFieldDefaults.colors(
+            disabledTextColor = Color.Transparent,
+            focusedContainerColor = containerColor,
+            unfocusedContainerColor = containerColor,
+            disabledContainerColor = containerColor,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+        ),
+        leadingIcon = leadingIcon,
+        value = password,
+        shape = RoundedCornerShape(15.dp),
+        onValueChange = { password = it },
+        singleLine = true,
+        placeholder = { Text("Password") },
+        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val image = if (passwordVisible)
+                Icons.Filled.Close
+            else Icons.Filled.Check
+
+            val description = if (passwordVisible) "Hide password" else "Show password"
+
+            IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                Icon(imageVector = image, description)
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun CustomButton() {
+    IconButton(
+
+        modifier = Modifier
+            .background(
+                color = colorResource(id = R.color.primary),
+                shape = CircleShape
+            ),
+        onClick = { /*TODO*/ }
+    ) {
+        Icon(painter = painterResource(id = R.drawable.ic_at), contentDescription = "Google")
+    }
+}
 
 @Composable
 fun Login(navController: NavController) {
@@ -116,7 +173,6 @@ fun Login(navController: NavController) {
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
-
         )
         {
             Image(
@@ -136,16 +192,69 @@ fun Login(navController: NavController) {
                     .padding(20.dp)
             )
             {
-                Text(text = "fdsfd")
-                RoundedCornerTextField()
-                Button(onClick = {
-                    navController.popBackStack()
-                    navController.navigate(MobileNavigationScreen.NavRoot.route)
-                }) {
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                ){
+                    CustomButton()
+                    Spacer(modifier = Modifier.width(16.dp))
                     Text(text = "Login")
                 }
+                Spacer(modifier = Modifier.height(100.dp))
+
+                CustomTextField(keyboardType = KeyboardType.Email, leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_at),
+                        contentDescription = "Email"
+                    )
+                }, placeholder = "Email")
+                Spacer(modifier = Modifier.height(16.dp))
+                CustomPasswordTextField(leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_key),
+                        contentDescription = "Email"
+                    )
+                })
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                CustomButton(text = "Entrar", onClick = {
+                    navController.popBackStack()
+                    navController.navigate(MobileNavigationScreen.NavRoot.route)
+                })
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                CustomButton(
+                    backgroundColor = colorResource(id = R.color.secondary),
+                    text = "Login com o Google",
+                    onClick = {
+                        navController.popBackStack()
+                        navController.navigate(MobileNavigationScreen.NavRoot.route)
+                    })
             }
         }
+    }
+}
+
+@Composable
+@Preview
+fun CustomButton(
+    text: String = "Button",
+    onClick: () -> Unit = { },
+    backgroundColor: Color = colorResource(id = R.color.primary),
+) {
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor
+        ),
+        shape = RoundedCornerShape(20.dp),
+
+        onClick = onClick,
+        modifier = Modifier
+            .height(60.dp)
+            .fillMaxWidth()
+    ) {
+        Text(text = text, color = colorResource(id = R.color.black))
     }
 }
 
