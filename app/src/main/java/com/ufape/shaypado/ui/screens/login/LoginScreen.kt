9075e579dafb761af919416fe2d18ec998cddfca
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2024-02-09 19:39:29 -0300

    Added LoginScreen layout

--- app/src/main/java/com/ufape/shaypado/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/ufape/shaypado/ui/screens/login/LoginScreen.kt
@@ -1,7 +1,7 @@
 package com.ufape.shaypado.ui.screens.login
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,25 +13,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ufape.shaypado.R
 import com.ufape.shaypado.ui.components.AppText
 import com.ufape.shaypado.ui.components.AppButton
-import com.ufape.shaypado.ui.components.RoundedButton
 import com.ufape.shaypado.ui.components.CustomTextField
 import com.ufape.shaypado.ui.components.TextType
 import com.ufape.shaypado.ui.routes.AuthNavigationScreen
 import com.ufape.shaypado.ui.routes.MobileNavigationScreen
-import com.ufape.shaypado.ui.theme.AtIcon
+import com.ufape.shaypado.ui.theme.EmailIcon
 import com.ufape.shaypado.ui.theme.GoogleImage
 import com.ufape.shaypado.ui.theme.KeyIcon
 import com.ufape.shaypado.ui.theme.ShaypadoImage
@@ -42,9 +38,9 @@ fun LoginScreen(navController: NavController) {
         modifier = Modifier
             .fillMaxSize()
             .background(
-                color = MaterialTheme.colorScheme.primary,
+                color = MaterialTheme.colorScheme.surface,
             )
-            .padding(top = 102.dp)
+            .padding(top = 24.dp)
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -53,40 +49,40 @@ fun LoginScreen(navController: NavController) {
         {
             ShaypadoImage()
 
-            Spacer(modifier = Modifier.height(80.dp))
+            Spacer(modifier = Modifier.height(24.dp))
 
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(
                         color = MaterialTheme.colorScheme.background,
-                        shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
+                        shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp)
                     )
-                    .padding(20.dp)
+                    .padding(16.dp)
             )
             {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    RoundedButton(onClick = {
-                        navController.popBackStack()
-                    })
-                    Spacer(modifier = Modifier.width(16.dp))
-                    AppText(TextType.TITLE_MEDIUM, text = R.string.sign_in_title)
-                }
-                Spacer(modifier = Modifier.height(100.dp))
+                AppText(TextType.HEADLINE_MEDIUM, text = R.string.sign_in_title)
+
+                Spacer(modifier = Modifier.height(32.dp))
+
                 CustomTextField(
                     keyboardType = KeyboardType.Email,
-                    leadingIcon = { AtIcon() },
-                    placeholder = R.string.input_email
+                    leadingIcon = { EmailIcon() },
+                    placeholder = R.string.input_email_placeholder,
+                    label = R.string.input_email
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 CustomTextField(
                     keyboardType = KeyboardType.Password,
                     leadingIcon = { KeyIcon() },
-                    placeholder = R.string.input_password
+                    placeholder = R.string.input_password_placeholder,
+                    label = R.string.input_password
                 )
-                Spacer(modifier = Modifier.height(50.dp))
+                TextButton(onClick = {
+                    navController.navigate(AuthNavigationScreen.ForgotPassword.route)
+                }) {
+                    AppText(TextType.BODY_MEDIUM, text = R.string.sign_up_forgot_password)
+                }
                 AppButton(
                     text = R.string.button_login,
                     onClick = {
@@ -97,9 +93,9 @@ fun LoginScreen(navController: NavController) {
                         }
                     }
                 )
-                Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = Modifier.height(16.dp))
                 AppButton(
-                    backgroundColor = MaterialTheme.colorScheme.secondary,
+                    backgroundColor = MaterialTheme.colorScheme.tertiaryContainer,
                     text = R.string.button_google,
                     onClick = {
                         navController.popBackStack()
@@ -110,24 +106,23 @@ fun LoginScreen(navController: NavController) {
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         GoogleImage()
-                        Spacer(modifier = Modifier.width(11.dp))
-                        AppText(textType = TextType.LABEL_MEDIUM, R.string.button_google)
+                        Spacer(modifier = Modifier.width(8.dp))
+                        AppText(textType = TextType.TITLE_MEDIUM, R.string.button_google, color = MaterialTheme.colorScheme.onPrimaryContainer)
                     }
                 }
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(
-                    stringResource(id = R.string.sign_up_forgot_password),
-                    style = MaterialTheme.typography.labelSmall.copy(
-                        color = MaterialTheme.colorScheme.primary
-                    ),
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            navController.navigate(AuthNavigationScreen.ForgotPassword.route)
-                        }
-                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    AppText(TextType.BODY_MEDIUM, text = R.string.sign_up_no_account )
+                    TextButton(onClick = {
+                        navController.navigate(AuthNavigationScreen.SignUp.route)
+                    }) {
+                        AppText(TextType.BODY_MEDIUM, text = R.string.sign_up_label, color = MaterialTheme.colorScheme.onSurface)
+                    }
+                }
             }
         }
     }
